184a70a26e26865a3ff2b5f702185e06f68b1847
faux123 <reioux@gmail.com>
2012-09-26 20:28:59 -0500

    cpufreq/cpufreq_intellidemand: intellidemand cpufreq governor (version 3.1)
    Intellidemand is inspired by ondemand governor. Its main features are:
    
     - auto cpu hotplugging based on runnable thread average calculations
     - detection of browsing mode (long screen on activities with moderate run
       queue depth =< 38 )
     - limit max frequencies when in browsing mode to conserve battery for long
       duration tasks
    
    Browsing State Determination:
     1- Browsing state is enabled as power-on default. Browsing state is disabled
        when run queue depth is > 38 for 2 consecutive samples
        (very high instantaneous spikes)
    
    Browsing State Limit Max Freq Logic (sliding window algorithm):
     1- While Screen is ON > 3 min, and average load of the CPU is < 35%, then
        CPU Max frequencies will remain power-on default
     2- While Screen is On, if average CPU load > 35%, both CPUs max frequencies
        will be limited to a lower user configurable max value
     3- While Screen is On, After both CPUs' Max is limited to lowered max, when
        the average CPU load has dropped down to less than < 25%, then both CPUs
        Max freq will be restored to back to power-on default
    
    Runnable Thread Auto-hotplug (fast hotplug / slow unplug algorithm):
     1- When average number of running thread is < threshold, 2nd cpu will be
        unplugged.
     2- When > threshold, 2nd cpu will be hotplugged.
     3- The logic applies a fast hotplug and slow unplug algorithm so 2nd core
        will stay enabled longer to help deal with performance spikes
    
    changes from Ver 3.0 -> Ver 3.1:
      - add run queue persistence to better determine browsing state
    changes from Ver 2.0 -> Ver 3.0:
      - remove GPU Busy dependency for browsing state detection (this will make
        the governor more portable to other platforms without specific GPU
        architecture knowledge)
    
    dependencies:
      -- Qualcomm's run queue average logic (SOC independent logic)
      -- TI's runnable thread average logic (SOC independent logic)
    
    adapted for OMAP2 platform
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-omap2/board-tuna.c
    	arch/arm/mach-omap2/omap2plus-cpufreq.c
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -69,6 +69,9 @@ static bool omap_cpufreq_ready;
 static bool omap_cpufreq_suspended;
 
 static int oc_val;
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+extern bool lmf_screen_state;
+#endif
 
 static unsigned int omap_getspeed(unsigned int cpu)
 {
@@ -271,6 +274,9 @@ static void omap_cpu_early_suspend(struct early_suspend *h)
 
 	mutex_lock(&omap_cpufreq_lock);
 
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+	lmf_screen_state = false;
+#endif
 	if (screen_off_max_freq) {
 		max_capped = screen_off_max_freq;
 
@@ -288,6 +294,9 @@ static void omap_cpu_late_resume(struct early_suspend *h)
 
 	mutex_lock(&omap_cpufreq_lock);
 
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+	lmf_screen_state = true;
+#endif
 	if (max_capped) {
 		max_capped = 0;
 
