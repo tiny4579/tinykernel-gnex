c87532e6ce42c62dceae573d52e9cd8d185815b5
Andrew Bartholomew <andrewb03@gmail.com>
2012-11-06 21:05:07 -0500

    cpufreq: ondemand: set boostfreq to 700Mhz

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -125,7 +125,7 @@ static struct dbs_tuners {
 	.ignore_nice = 0,
 	.powersave_bias = 0,
 	.freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
-	.boostfreq = 0,
+	.boostfreq = 700000,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
